a3c25dc857942689f32c54618c4a2628a0c5936c
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-25 07:02:41 -0500

    Complete Section 1.4

---
+++ 1_Basics/4_Dynamic_Typing.js
@@ -0,0 +1,7 @@
+let person = 'Jaime'; // String Literal
+console.log(typeof (person));
+
+// Dynamic typing allows for type to be changed
+// At run time the type is determined
+person = 1;
+console.log(typeof (person))
