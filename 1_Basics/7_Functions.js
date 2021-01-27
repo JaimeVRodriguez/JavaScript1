843a6953fe8090beb225c337911aec6ef007af2c
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 15:35:59 -0500

    Complete Section 1.7

---
+++ 1_Basics/7_Functions.js
@@ -0,0 +1,14 @@
+function greet() {
+    console.log('Hello World');
+}
+// "Calling" the function
+greet()
+
+
+// Function with input
+// "name" is the PARAMETER  when creating the function
+function realGreeting(name) {
+    console.log('Hello ' + name);
+}
+// "name" is the ARGUMENT when using the function
+realGreeting('Jaime')
