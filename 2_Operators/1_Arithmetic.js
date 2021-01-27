4145d0e81f1b7b4dc776ee2f986ffcb1c5d10e3c
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 17:38:19 -0500

    Complete Section 2.1

---
+++ 2_Operators/1_Arithmetic.js
@@ -0,0 +1,18 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// Increment operator
+console.log(++x); // Incremented by one first
+
+console.log(y++); // Incremented after 
+console.log(y); // then it's changed the next time it's used
+
+// Decrement operator
+console.log(--x)
