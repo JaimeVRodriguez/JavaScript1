13dc89a976e00d666ab68d8b805da66e04c4f8bb
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 05:46:49 -0500

    Complete Section 1.5

---
+++ 1_Basics/5_Objects.js
@@ -0,0 +1,18 @@
+// Reference Types
+// Objects, Arrays, Functions
+
+// When you have variables that are all in the same "group" together
+let person = {
+    name: 'Jaime',
+    age: 30
+};
+
+console.log(person)
+
+// Dot notation
+person.name = 'Gunner';
+console.log(person.name)
+
+// Bracket Notation
+person['name'] = 'Krystal'
+console.log(person.name)
