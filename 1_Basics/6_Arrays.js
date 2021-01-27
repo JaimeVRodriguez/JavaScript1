d4fb83caf4e0d2455d9ae8cd0c0f1e5304c1d67c
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 15:30:31 -0500

    Complete Section 1.6

---
+++ 1_Basics/6_Arrays.js
@@ -0,0 +1,11 @@
+let selectedColors = ['red', 'blue'];
+
+selectedColors[2] = 'green';
+// Elements of array can be different types
+// selectedColors[3] = 1;
+
+let arrayLength = selectedColors.length;
+
+console.log(selectedColors);
+console.log(selectedColors[0]);
+console.log(arrayLength);
