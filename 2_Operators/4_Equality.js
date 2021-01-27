3e681ab07c0b98f2d75aa3e4d4ee3be454e5a6c4
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 18:18:46 -0500

    Complete Section 2.4

---
+++ 2_Operators/4_Equality.js
@@ -0,0 +1,9 @@
+// Strict equality 
+// Same type and value
+console.log(1 === 1);
+console.log('1' === 1); // Will be false because type is different
+
+
+// Loose equality
+console.log(1 == 1);
+console.log('1' == 1); // Will still be true because type does not matter
