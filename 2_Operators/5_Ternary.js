159fae7b7d5df990b476ee4fdfbd2f8d5d83128e
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-27 18:23:04 -0500

    Complete Section 2.5

---
+++ 2_Operators/5_Ternary.js
@@ -0,0 +1,12 @@
+// If customer has more than 100 points
+// customer is gold
+// otherwise they are silver
+
+let points = 110;
+
+// Reads as follows
+// if condition is true "? then" value is gold
+// ": if not" value is silver
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type)
