50d63fc8c22e5cd70dde634f7adee06756cce5b3
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-28 18:33:10 -0500

    Complete Section 2.8

---
+++ 2_Operators/7_Bitwise.js
@@ -0,0 +1,5 @@
+// 1 = 00000001   8 bits = 1 byte
+// 2 = 00000010
+
+console.log(1 | 2);
+console.log(1 & 2);
---
+++ 8_Exercise.js
@@ -0,0 +1,11 @@
+// Swap values of a and b
+
+let a = 'red';
+let b = 'blue';
+
+let x = a;
+a = b;
+b = x;
+
+console.log('A', a);
+console.log('B', b);
