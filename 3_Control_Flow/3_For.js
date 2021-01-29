a94018beaef9e15b7df6a38770220ed39a3242a0
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-01-28 19:06:58 -0500

    Complete Section 3.3

---
+++ 3_Control_Flow/3_For.js
@@ -0,0 +1,15 @@
+// Display something 5 times on the console
+
+for (let i = 0; i < 5; i++) {
+    console.log('Hello World', i);
+}
+
+// Display odd numbers from 1-5
+for (let i = 0; i <= 5; i++) {
+    if (i % 2 !== 0)
+        console.log(i)
+}
+
+// console.log can be on the same line as the if statement
+// follow best practice for your organization
+
