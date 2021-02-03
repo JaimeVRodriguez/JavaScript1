43d6bdfdf66b6dcbf60af6800f9c38641a9a9a8a
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-02 20:24:02 -0500

    Complete Section 3.8

---
+++ 3_Control_Flow/8_Exercise2.js
@@ -0,0 +1,10 @@
+// Determine if dimensions are landscape or portrait
+
+function isLandscape(width, height) {
+
+    return (width > height);
+
+}
+
+console.log(isLandscape(30, 12));
+console.log(isLandscape(14, 21));
