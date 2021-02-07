9982ebd4f79244992b3f523165f0bbf8b33deaa5
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-06 19:16:22 -0500

    Complete Section 4.2

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius, // equivalent to radius: radius,
+
+        draw() {
+            console.log('Radius', circle.radius);
+        }
+    };
+}
+
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2)
