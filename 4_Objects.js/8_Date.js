418197f09ec4d393cce49ea86068917511c93df2
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-08 18:44:06 -0500

    Complete Section 4.8

---
+++ 4_Objects.js/8_Date.js
@@ -0,0 +1,9 @@
+const now = new Date();
+
+// Google Javascript Date for supported date formats
+const date1 = new Date('May 11 2018 09:00');
+
+const date2 = new Date(2018, 4, 11, 9);
+
+console.log(now.getMonth());
+console.log(now.toDateString());
